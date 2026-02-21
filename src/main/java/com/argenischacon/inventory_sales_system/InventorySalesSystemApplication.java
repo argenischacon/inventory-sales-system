a6da30df8329e603d471d1d48d517f249c5aad48
argenischacon <argenischaconb@gmail.com>
2026-02-21 19:10:11 -0400

    first commit

---
+++ src/main/java/com/argenischacon/inventory_sales_system/InventorySalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.argenischacon.inventory_sales_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySalesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argenischacon/inventory_sales_system/InventorySalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argenischacon.inventory_sales_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySalesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
